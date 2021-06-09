aa32c3eebf54f99b9d34b9430755c5f4a4535554
Paulson McIntyre <pmcintyre@salesforce.com>
2021-06-09 16:31:04 -0400

    Switching to s3

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -5,7 +5,6 @@ try:
     import IPython.lib
     from hybridcontents import HybridContentsManager
     from s3contents import S3ContentsManager, GCSContentsManager
-    from pgcontents.pgmanager import PostgresContentsManager
     # LargeFileManager is the default Jupyter content manager
     # NOTE: LargFileManager only exists in notebook > 5
     # If using notebook < 5, use FileContentManager instead
@@ -22,60 +21,56 @@ try:
         c.NotebookApp.token = ''
         c.NotebookApp.password = ''
 
-    ### PostresContentsManager ###
-    database_url = os.getenv('DATABASE_URL', None)
-    if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
-    if database_url:
-        # Tell IPython to use PostgresContentsManager for all storage.
-        c.NotebookApp.contents_manager_class = HybridContentsManager
+    # Tell IPython to use PostgresContentsManager for all storage.
+    c.NotebookApp.contents_manager_class = HybridContentsManager
 
-        c.HybridContentsManager.manager_classes = {
-            # # Associate the root directory with a LargeFileManager,
-            # # This manager will receive all requests that don't fall under any of the
-            # # other managers.
-            # # If you want to make this path un-editable you can configure it to use a read-only filesystem
-            # '': LargeFileManager,
-            # # Associate /directory with a LargeFileManager.
-            # 'directory': LargeFileManager,
-            # Associate the postgres directory with a PostgresContentManager
-            '': PostgresContentsManager,
-            # Associate the s3 directory with AWS S3
-            's3': S3ContentsManager,
-            # # Associate the gcs directory with GCS
-            # 'gcs': GCSContentsManager
-        }
+    c.HybridContentsManager.manager_classes = {
+        # # Associate the root directory with a LargeFileManager,
+        # # This manager will receive all requests that don't fall under any of the
+        # # other managers.
+        # # If you want to make this path un-editable you can configure it to use a read-only filesystem
+        # '': LargeFileManager,
+        # # Associate /directory with a LargeFileManager.
+        # 'directory': LargeFileManager,
+        # Associate the postgres directory with a PostgresContentManager
+        # 'postgres': PostgresContentsManager,
+        # Associate the s3 directory with AWS S3
+        '': S3ContentsManager,
+        # # Associate the gcs directory with GCS
+        # 'gcs': GCSContentsManager
+    }
 
-        c.HybridContentsManager.manager_kwargs = {
-            # # Args for the LargeFileManager mapped to /directory
-            # '': {
-            #     'root_dir': '/tmp/read-only',
-            # },
-            # # Args for the LargeFileManager mapped to /directory
-            # 'directory': {
-            #     'root_dir': '/home/viaduct/local_directory',
-            # },
-            # Args for  PostgresContentsManager.
-            '': {
-                'db_url': database_url,
-                'max_file_size_bytes': 1024 * 1024 * 1024 * 2,  # Optional
-            },
-            # Args for  S3ContentManager.
-            's3': {
-                "access_key_id": os.environ.get("BUCKETEER_AWS_ACCESS_KEY_ID", ""),
-                "secret_access_key": os.environ.get("BUCKETEER_AWS_SECRET_ACCESS_KEY", ""),
-                "endpoint_url": "https://s3." + os.environ.get("BUCKETEER_AWS_REGION", "us-east-1") + ".amazonaws.com",
-                "bucket": os.environ.get("BUCKETEER_BUCKET_NAME", ""),
-                "prefix": os.environ.get("BUCKETEER_BUCKET_NAME", ""),
-            },
-            # # Args for  GCSContentManager.
-            # 'gcs': {
-            #     'project': "<your-project>",
-            #     'token': "~/.config/gcloud/application_default_credentials.json",
-            #     'bucket': "<bucket-name>"
-            # },
-        }
-        c.PostgresContentsManager.user_id = 'heroku'
+    c.HybridContentsManager.manager_kwargs = {
+        # # Args for the LargeFileManager mapped to /directory
+        # '': {
+        #     'root_dir': '/tmp/read-only',
+        # },
+        # # Args for the LargeFileManager mapped to /directory
+        # 'directory': {
+        #     'root_dir': '/home/viaduct/local_directory',
+        # },
+        # Args for  PostgresContentsManager.
+        # 'postgres': {
+        #     'db_url': database_url,
+        #     'max_file_size_bytes': 1024 * 1024 * 1024 * 2,  # Optional
+        # },
+        # Args for  S3ContentManager.
+        '': {
+            "access_key_id": os.environ.get("BUCKETEER_AWS_ACCESS_KEY_ID", ""),
+            "secret_access_key": os.environ.get("BUCKETEER_AWS_SECRET_ACCESS_KEY", ""),
+            "endpoint_url": "https://s3." + os.environ.get("BUCKETEER_AWS_REGION", "us-east-1") + ".amazonaws.com",
+            "bucket": os.environ.get("BUCKETEER_BUCKET_NAME", ""),
+            "prefix": os.environ.get("BUCKETEER_BUCKET_NAME", ""),
+        },
+        # # Args for  GCSContentManager.
+        # 'gcs': {
+        #     'project': "<your-project>",
+        #     'token': "~/.config/gcloud/application_default_credentials.json",
+        #     'bucket': "<bucket-name>"
+        # },
+    }
+    c.S3ContentsManager.user_id = 'heroku'
+
 
     def no_spaces(path):
         return ' ' not in path
@@ -83,7 +78,8 @@ try:
 
     c.HybridContentsManager.path_validators = {
         'postgres': no_spaces,
-        # 's3': no_spaces
+        's3': no_spaces,
+        '': no_spaces,
     }
 
 except Exception:
